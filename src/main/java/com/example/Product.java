fb2d9c0312e0eabde94e8f01225db75664b80984
“cristinabustamanterunde” <cristina.bustamanterunde@gmail.com>
2023-01-17 10:38:59 +0100

    Creación Productos

--- src/main/java/com/example/Product.java
+++ src/main/java/com/example/Product.java
@@ -1,5 +1,16 @@
 package com.example;
 
 public class Product {
+    public static void main(String[] args) {
+
+    // Artículos de bisutería
+
+        String Product1 = "Broche";
+        String product2 = "Pendiente";
+        String Product3 = "Pulsera";
+        String Product4 = "Gargantilla";
+        String Product5 = "Aro";
+
+    }
 }
 
