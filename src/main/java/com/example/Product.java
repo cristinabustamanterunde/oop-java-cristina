553d7d9b8288f528dc7c2abe41b0e78c7b1cdc89
“cristinabustamanterunde” <cristina.bustamanterunde@gmail.com>
2023-01-17 22:29:03 +0100

    Corrección 2

--- src/main/java/com/example/Product.java
+++ src/main/java/com/example/Product.java
@@ -1,14 +1,22 @@
 package com.example;
 
-import java.security.PrivateKey;
+import jdk.jfr.Category;
 
 public class Product {
 
+    //* Artículos de bisutería
+
     private String name;
-    private Double Price;
-    private String Manufacturer;
+    private Double price;
+    private Category category;
+    private String material;
+    private Float size;
+    private Integer unitsAvailable;
 
 
+
+    public Product() {
     }
+}
 }
 
